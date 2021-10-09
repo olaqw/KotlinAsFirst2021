200976f1ecbfbc07cab435af635e330b25ee2fcb
olaqw <alex925489@gmail.com>
2021-10-09 11:25:51 +0300

    4 урок 4 задачи

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -120,14 +120,28 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double =
+    when {
+        v.isEmpty() -> 0.0
+        else -> {
+            var s = 0.0
+            for (i in 0 until v.size)
+                s += v[i] * v[i]
+            sqrt(s)
+        }
+    }
+
 
 /**
  * Простая (2 балла)
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double =
+    when {
+        list.isEmpty() -> 0.0
+        else -> list.sum() / list.size
+    }
 
 /**
  * Средняя (3 балла)
@@ -137,7 +151,16 @@ fun mean(list: List<Double>): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> =
+    when {
+        list.isEmpty() -> list
+        else -> {
+            val c = mean(list)
+            for (i in 0 until list.size)
+                list[i] = list[i] - c
+            list
+        }
+    }
 
 /**
  * Средняя (3 балла)
@@ -146,7 +169,16 @@ fun center(list: MutableList<Double>): MutableList<Double> = TODO()
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.
  */
-fun times(a: List<Int>, b: List<Int>): Int = TODO()
+fun times(a: List<Int>, b: List<Int>): Int =
+    when {
+        a.isEmpty() || b.isEmpty() -> 0
+        else -> {
+            var c = 0
+            for (i in 0 until a.size)
+                c += a[i] * b[i]
+            c
+        }
+    }
 
 /**
  * Средняя (3 балла)
