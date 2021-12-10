ede7d71f2a575d548edc191b5c9e279ec849105e
olaqw <alex925489@gmail.com>
2021-12-10 12:43:20 +0300

    2312

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -186,7 +186,10 @@ fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<S
  *   averageStockPrice(listOf("MSFT" to 100.0, "MSFT" to 200.0, "NFLX" to 40.0))
  *     -> mapOf("MSFT" to 150.0, "NFLX" to 40.0)
  */
-fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Unit> = TODO()
+fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> =
+    stockPrices.groupBy { it.first }.mapValues { it: Map.Entry<String, List<Pair<String, Double>>> ->
+        it.value.size / it.value.sumOf { it.second }
+    }
 
 /**
  * Средняя (4 балла)
